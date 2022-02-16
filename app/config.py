c944a3fdb5ad811123cd71de1b57525eea018194
UppercutsFromKupchino <mitya.kalmykov.01@mail.ru>
2022-02-16 16:10:55 +0300

    biba

--- app/config.py
+++ app/config.py
@@ -1,9 +1,9 @@
 class Config:
-    db_name = 'Konshina_BD'
-    username = 'postgres'
+    db_name = 'dbaq2qoff7h4l3'
+    username = 'ijxaoyrtuckmnx'
     dialect = 'postgresql'
-    password = 'alp37327'
-    host = 'localhost'
+    password = '6b55b57f8f183632ed5f8902fcdcf54546d0129a7c947f62f205004dbf1b8367'
+    host = 'ec2-54-155-208-5.eu-west-1.compute.amazonaws.com'
 
     SQLALCHEMY_DATABASE_URI = f'{dialect}://{username}:{password}@{host}/{db_name}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
